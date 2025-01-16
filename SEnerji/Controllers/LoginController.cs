61c3828ed06865785bde58f60c4dad8f352face1
Sevgi <86626202+sevgitry@users.noreply.github.com>
2025-01-16 14:05:24 +0300

    hiçbişi

--- SEnerji/Controllers/LoginController.cs
+++ SEnerji/Controllers/LoginController.cs
@@ -1,14 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataBaseLayer;
+using EntityLayer;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace SEnerji.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ApplicationDbContext _context = new();
+        [HttpPost]
         public IActionResult Index()
         {
             return View();
         }
-       public IActionResult SignUp()
+            public IActionResult SignUp()
             {
                 return View();
             }
